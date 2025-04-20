77fb73ae90275fc2e1ecd065d3e555e09adc1a36
Prajwal Korban <128471316+prajjukorban@users.noreply.github.com>
2025-04-20 14:33:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = socketIo(server);
 
 // Connect to MongoDB (update your connection string)
 // mongodb+srv://dbuser:ka33k9449@secretspeak.gf3w9.mongodb.net/chating
-mongoose.connect('mongodb+srv://prajjukorban:Ka33k9449@loginpractice.pdgsqb9.mongodb.net/?retryWrites=true&w=majority&appName=loginpractice', {
+mongoose.connect('mongodb+srv://prajjukorban:Ka33k9449@loginpractice.pdgsqb9.mongodb.net/chatting', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
